b4503aa58fb01745703619a15204df94b74d4dc7
JunlinGong <913756523@qq.com>
2023-05-12 21:58:19 +0800

    feat: 引入tailwind css 和 logo图片

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.png';
 
 function App() {
   return (
-    <div>456</div>
+    <div className='box_wrapper m-2'>
+      <div className='logo'>
+        <img src={logo} alt="xxxs" />
+      </div>
+      <div className='search_box'>
+        <input type="text" />
+        <button>按钮</button>
+      </div>
+    </div>
   );
 }
 
